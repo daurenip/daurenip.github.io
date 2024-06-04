ded44297782285dbb3edb8a05bb9bb7aa3ba33bc
daurenip <daurenserikkaliev1999@gmail.com>
2024-06-04 14:32:53 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,18 @@ function disableRightClick(event) {
 }
 document.addEventListener('contextmenu', disableRightClick);
 
-function handleClipboardChange(event) {
-    var clipboardData = event.clipboardData || window.clipboardData;
-    var clipboardItems = clipboardData.items;
-    
-    for (var i = 0; i < clipboardItems.length; i++) {
-        var item = clipboardItems[i];
-        
-        if (item.type.indexOf('image') !== -1) {
-            alert("Обнаружена попытка создания скриншота страницы.");
-            break;
-        }
+document.onkeydown = function(e) {
+    if(event.keyCode == 123) {
+    return false;
     }
-}
-
-document.addEventListener('paste', handleClipboardChange);
-
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+    return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+    return false;
+    }
+    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+    return false;
+    }
+    }
+    
